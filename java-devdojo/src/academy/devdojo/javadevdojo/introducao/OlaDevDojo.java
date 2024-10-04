8b1577846f53a7adcfe7be974d7b6ef602849768
coutbiaafnts <bia.fontes653@gmail.com>
2024-10-04 15:05:44 -0300

    feat: tipos de comentários

--- java-devdojo/src/academy/devdojo/javadevdojo/introducao/OlaDevDojo.java
+++ java-devdojo/src/academy/devdojo/javadevdojo/introducao/OlaDevDojo.java
@@ -1,7 +1,20 @@
 package academy.devdojo.javadevdojo.introducao;
 
 public class OlaDevDojo {
+    /**
+     * Isso é um comentário <b>javadoc</b>
+     * @param args
+     */
     public static void main(String[] args) {
+
+        // Isso é um comentário de uma linha
+
+        /*
+        Isso é um
+        comentário de
+        multiplas linhas
+         */
+
         System.out.println("Hello World!");
     }
 }
