e3c55e02f6e8a96ae7942338163546ce7bbbd3a6
coutbiaafnts <bia.fontes653@gmail.com>
2024-10-04 16:36:28 -0300

    feat: tipos primitivos pt 2 declaração e tamanho em memória

--- java-devdojo/src/academy/devdojo/javadevdojo/introducao/Class02PrimitiveTypes.java
+++ java-devdojo/src/academy/devdojo/javadevdojo/introducao/Class02PrimitiveTypes.java
@@ -4,7 +4,26 @@ public class Class02PrimitiveTypes {
     public static void main(String[] args) {
 
         // int, double, float, char, byte, short, long e boolean
-        int userAge = 21;
-        System.out.println("A idade do usuário é " + userAge + " anos!");
+        int userAgeInt = 21;
+        long bigNumber = 199992;
+        double userWageDouble = 2000.90;
+        float userWageFloat = 2550.50f;
+        byte userAgeByte = 127;
+        short userAgeShort = 22;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char userSex = 'F';
+        char numberChar = 87; //w
+
+        System.out.println("Idade em int: " + userAgeInt);
+        System.out.println("Uma número grande: " + bigNumber);
+        System.out.println("Salário em double: " + userWageDouble);
+        System.out.println("Salário em float: " + userWageFloat);
+        System.out.println("Idade em byte: " + userAgeByte);
+        System.out.println("Idade em short: " + userAgeShort);
+        System.out.println("É verdadeiro? " + isTrue);
+        System.out.println("É falso? " + isFalse);
+        System.out.println("Sexo do usuário: " + userSex);
+        System.out.println("Caractere por número: " + numberChar);
     }
 }
